2ba0e4cb1d15167b6064c052d6932dd65acbf06e
algo-vaultstaking <vaultstaking@gmail.com>
2023-02-02 09:42:19 -0500

    updates

--- main_schedule.py
+++ main_schedule.py
@@ -45,19 +45,30 @@ async def check_thresholds():
             threshold: float = database.get_threshold_by_network(db_connection, network, guild)
             addresses = database.get_all_addresses_by_network(db_connection, network, guild)
             for address in addresses:
+                try:
+                    balance: float = database.get_balance(network, address)
+                except Exception as e:
+                    print(e)
+                    db_connection.close()
+                    return False
 
-                balance: float = database.get_balance(network, address)
                 database.update_balance(db_connection, network, address, balance)
+
                 alerting = database.get_alerting_by_address(db_connection, network, address)
                 token_abr = database.get_token_abr_by_network(db_connection, network)
 
                 if (balance < threshold) and not alerting:
                     contacts = database.get_contacts_by_address(db_connection, address, guild)
                     if len(contacts) == 0 or contacts == "None":
+                        print(f"**{addresses[address]} ({address[:6]}...{address[-4:]})** is below the threshold of {threshold} "
+                              f"{token_abr}. It currently has a balance of {round(balance, 3)} {token_abr}.")
                         await threshold_channel.send(
                             f"**{addresses[address]} ({address[:6]}...{address[-4:]})** is below the threshold of {threshold} "
                             f"{token_abr}. It currently has a balance of {round(balance, 3)} {token_abr}.")
                     else:
+                        print(
+                            f"{contacts}, **{addresses[address]} ({address[:6]}...{address[-4:]})** is below the threshold of {threshold} "
+                            f"{token_abr}. It currently has a balance of {round(balance, 3)} {token_abr}.")
                         await threshold_channel.send(
                             f"{contacts}, **{addresses[address]} ({address[:6]}...{address[-4:]})** is below the threshold of {threshold} "
                             f"{token_abr}. It currently has a balance of {round(balance, 3)} {token_abr}.")
@@ -66,10 +77,16 @@ async def check_thresholds():
                 if (balance > threshold) and alerting:
                     contacts = database.get_contacts_by_address(db_connection, address, guild)
                     if len(contacts) == 0 or contacts == "None":
+                        print(
+                            f"**{addresses[address]} ({address[:6]}...{address[-4:]})** is back above the threshold of {threshold} "
+                            f"{token_abr}. It currently has a balance of {round(balance, 3)} {token_abr}.")
                         await threshold_channel.send(
                             f"**{addresses[address]} ({address[:6]}...{address[-4:]})** is back above the threshold of {threshold} "
                             f"{token_abr}. It currently has a balance of {round(balance, 3)} {token_abr}.")
                     else:
+                        print(
+                            f"{contacts}, **{addresses[address]} ({address[:6]}...{address[-4:]})** is back above the threshold of {threshold} "
+                            f"{database.get_token_abr_by_network(db_connection, network)}")
                         await threshold_channel.send(
                             f"{contacts}, **{addresses[address]} ({address[:6]}...{address[-4:]})** is back above the threshold of {threshold} "
                             f"{database.get_token_abr_by_network(db_connection, network)}")
@@ -77,7 +94,7 @@ async def check_thresholds():
     db_connection.close()
     await bot.change_presence(
         activity=discord.Activity(type=discord.ActivityType.watching, name=f"the time: {now.strftime('%H:%M, %m/%d')}"))
-    return
+    return True
 
 
 bot.run(discord_token)
